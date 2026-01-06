4cde58e81e9f70edcae196b751089112e5c4f17d
Brevin Monks <b00162915@mytudublin.ie>
2026-01-06 19:42:29 +0000

    Create MediaCard.jsx
    Card component for displaying posts

---
+++ soc-app/src/app/components/MediaCard.jsx
@@ -0,0 +1,40 @@
+import Card from '@mui/materials/Card';
+import CardContent from '@mui/materials/Card';
+import CardHeader from '@mui/materials/Card';
+import CardActions from '@mui/materials/Card';
+import Typography from '@mui/materials/Card';
+
+function timeSincePost(date){
+    const seconds = Math.floor((Date.now() - date.getTime() / 1000))
+
+    const timeIntervals= [
+        {label: "year", seconds:31536000},
+        {label: "month", seconds:2592000},
+        {label: "day", seconds:86400},
+        {label: "hour", seconds:3600},
+        {label: "minute", seconds:60}
+    ]
+
+    for(const timeInterval of timeIntervals){
+        const count = Math.floor(seconds / timeInterval.seconds)
+
+        return '${count} ${interval.label}s ago'
+    }
+}
+
+export default function MediaCard({username, timePosted, title, likes, comments}){
+    return (
+        <Card sx={{maxWidth: 500, mb: 2}}>
+            <CardHeader title={username} sub header={timeSincePost(timePosted)}/>
+
+            <CardContent>
+                <Typography variant="h6">{title}</Typography>
+            </CardContent>
+
+            <CardActions>
+                <Typography variant="body2">Likes: {likes}</Typography>
+                <Typography variant="body2">Comments: {comments}</Typography>
+            </CardActions>
+        </Card>
+    )
+}
